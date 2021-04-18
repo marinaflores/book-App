b2daf567948c797c409ac05c359213cee08ce4d1
marinaflores <marinacaseflor@gmail.com>
2021-04-18 10:08:34 -0300

    Adicionando o Booking

---
+++ app/models/booking.rb
@@ -0,0 +1,15 @@
+class Booking < ActiveRecord::Base
+  belongs_to :user
+
+  attr_accessor :current_user
+
+  validates_presence_of :date, :hour
+  validate :ownership, on: [:update]
+
+  private
+
+  def ownership
+    errors.add(:user, 'Não poder alterar a reserva de outro usuário') if user != current_user
+  end
+
+end
